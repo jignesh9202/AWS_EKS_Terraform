67dbf81b84e64720b35a7361bd82291a6904a56b
Jignesh <jignesh.9202@gmail.com>
2022-09-22 22:28:25 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+# --- root/main.tf ---
+
+module "vpc" {
+  source           = "./vpc"
+  vpc_cidr         = "10.1.0.0/16"
+  max_subnet       = 2
+  public_sn_count  = 2
+  access_ip        = "0.0.0.0/0"
+}
+
+module "eks" {
+  source         = "./eks"
+  vpc_id         = module.vpc.vpc_id
+  key_pair       = "my_key_pair"
+  instance_types = ["t3.small"]
+  desired_size   = 2
+  min_size       = 1
+  max_size       = 5
+  public_subnets = module.vpc.public_subnets
+}
---
+++ vpc/variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_cidr" {}
+variable "public_sn_count" {}
+variable "access_ip" {}
+variable "max_subnet" {}
+variable "suffix" {
+    default = "eks"
+}
